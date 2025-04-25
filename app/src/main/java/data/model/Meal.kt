a4c86cff6736d6b28b1beca3407aaf81797a23b0
M Hernan F <hfauzan47@gmail.com>
2025-04-26 03:38:21 +0700

    update bug search

--- app/src/main/java/data/model/Meal.kt
+++ app/src/main/java/data/model/Meal.kt
@@ -1,13 +1,18 @@
 package data.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Meal(
     val idMeal: String,
     val strMeal: String,
-    val strCategory: String,
-    val strArea: String,
-    val strInstructions: String,
-    val strMealThumb: String,
+    val strCategory: String?,
+    val strArea: String?,
+    val strInstructions: String?,
+    val strMealThumb: String?,
     val strTags: String?,
     val strYoutube: String?
-)
+) : Parcelable
+
 
